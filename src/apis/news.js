c824af2839069cd502046925c6dcb090367c3c6e
gh.kang <gh.kang@cyrexpay.com>
2024-03-22 18:00:36 +0900

    t

--- src/apis/news.js
+++ src/apis/news.js
@@ -1,6 +1,6 @@
 import axios from '@/apis/axios.js';
 
-export const getNews = async () => {
+export const fetchGetNews = async () => {
   try {
     return await axios.get('/news');
   } catch (e) {
--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,33 +1,18 @@
 import classes from './News.module.css';
 import Pagination from '../../components/pagination/Pagination.jsx';
 import NewsItem from '../../components/news/NewsItem.jsx';
-import { getNews } from '@/apis/news.js';
+import { fetchGetNews } from '@/apis/news.js';
 import { useEffect, useState } from 'react';
 
-const newsList2 = [
-  {
-    title: 'News Title',
-    description: 'description',
-    regDate: '2023-04-12',
-    img_uri: '',
-    link: '',
-  },
-  {
-    title: 'News Title2',
-    description: 'description2',
-    date: '2023-04-13',
-    img_uri: '',
-    link: '',
-  },
-];
 export default function News() {
   const [newsList, setNewsList] = useState(null);
   useEffect(() => {
     const fetchGetNewsList = async () => {
-      const { status, data } = await getNews();
+      const { status, data } = await fetchGetNews();
 
       if (status === 200) {
-        setNewsList(data.news.data);
+        // setNewsList(data.news.data);
+        setNewsList(null);
       }
     };
 
@@ -38,11 +23,14 @@ export default function News() {
       <div className="content">
         <div className={classes.contentWrapper}>
           <div className={classes.newsList}>
-            {newsList?.map((news) => (
-              <NewsItem news={news} key={news.uuid} />
-            ))}
+            {newsList && newsList.length > 0 ? (
+              newsList?.map((news) => <NewsItem news={news} key={news.uuid} />)
+            ) : (
+              <div className={classes.noData}>
+                피드 받으신 뉴스가 없습니다. 최근 7일 까지 등록한 피드가 기록되고 있습니다.
+              </div>
+            )}
           </div>
-
           <Pagination />
         </div>
       </div>
